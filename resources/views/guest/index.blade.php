e799c7d4e388b2da98a718a559e332432d7823fd
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-06 16:13:50 +0100

    Add about section

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -30,11 +30,12 @@
     </div>
 </nav>
 
+<!-- header section-->
 <section class="h-screen relative overflow-hidden bg-cover bg-center" style="background-image: url('{{ asset('images/bg-home-guest.png') }}');">
     <!-- Overlay with translucent white -->
     <div class="absolute inset-0 bg-white bg-opacity-50"></div>
     
-    <!-- Your content with adjusted text color -->
+    <!-- content welcome header-->
     <div class="relative z-10 flex flex-col justify-center items-center h-full text-center text-gray-900">
         <h1 class="text-4xl sm:text-5xl md:text-6xl font-bold mb-6">Beat Our Beets</h1>
         <p class="text-lg sm:text-xl md:text-2xl mb-8">Indulge in a unique culinary experience at our restaurant, <br>where every dish celebrates the vibrant and versatile beetroot in innovative and delicious ways.</p>
@@ -46,14 +47,14 @@
 </section>
 
 
-
+<!--menu section-->
 <section class="py-12 lg:py-24">
     <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-6 lg:px-0">
-        <!-- Picture on the left -->
+       <!--image-->
         <div class="lg:w-1/2 lg:px-8 mb-8 lg:mb-0">
             <img src="{{ asset('images/beets-menu-guest.jpg') }}" alt="Beet Dishes" class="w-full h-auto rounded-lg">
         </div>
-        <!-- Information on the right -->
+        
         <div class="lg:w-1/2 px-6 lg:px-12">
             <h2 class="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">Discover Our Beetsy Menu</h2>
             <p class="text-lg text-gray-700 mb-6">Explore our wide range of dishes crafted with the finest beets sourced from local farmers. From salads to main courses and desserts, we have something delightful for everyone.</p>
@@ -63,6 +64,7 @@
     </div>
 </section>
 
+<!--booking section-->
 <section class="bg-red-200 py-20">
     <div class="container mx-auto text-center">
         <h2 class="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">Book a Table</h2>
@@ -71,7 +73,24 @@
     </div>
 </section>
 
-
+<!--about section-->
+<section class="py-12 lg:py-24">
+    <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-6 lg:px-0">
+        <!--content menu-->
+        <div class="lg:w-1/2 px-6 lg:px-12">
+            <h2 class="text-3xl lg:text-4xl font-bold text-gray-800 mb-6">About Us</h2>
+            <p class="text-base lg:text-lg text-gray-700 mb-6">At Beets Restaurant, we celebrate the vibrant and versatile beetroot by crafting innovative and delicious dishes that delight the senses and nourish the soul.</p>
+        <p class="text-base lg:text-lg text-gray-700 mb-6">With locally sourced ingredients and a commitment to sustainability, every dish tells a story of quality, creativity, and dedication.</p>
+        <p class="text-base lg:text-lg text-gray-700 mb-6">Join us on a culinary journey where every bite is an exploration of flavor, texture, and imagination.</p>
+        <p class="text-base lg:text-lg text-gray-700 mb-6">Experience the magic of beets with us!</p>
+        </div>    
+        <!--image-->
+        <div class="lg:w-1/2 lg:px-4 mb-8 lg:mb-0">
+            <img src="{{ asset('images/bg-beets-on-plate.jpg') }}" alt="Beet Dishes" class="w-full  h-auto rounded-lg">
+        </div>
+        
+    </div>
+</section>
 
 
 
