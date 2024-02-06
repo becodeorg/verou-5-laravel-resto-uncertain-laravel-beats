56f4747292a0b0b508e14e9a48605b87b7dbd519
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-06 11:46:15 +0100

    Styling navbar

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -7,21 +7,31 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-   <nav class="bg-red-50 w-full flex justify-center pt-4 pb-4">
-  <div class="max-w-7xl px-2 sm:px-6 lg:px-8 flex justify-between items-center">
-    <div class="flex items-center">
-      <img class="h-8 w-auto pr-6" src="{{ asset('images/beets.png') }}" alt="Beets Restaurant">
-    </div>
+<nav class="bg-red-50 w-full flex justify-between items-center px-20 py-4 ">
+  <div class="flex items-center ">
+    <img class="h-8 w-auto pr-6" src="{{ asset('images/beets.png') }}" alt="Beets Restaurant">
     <div class="hidden sm:block">
       <div class="flex space-x-4">
-        <a href="#" class="text-gray-900 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-        <a href="#" class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium">Team</a>
-        <a href="#" class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-        <a href="#" class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+        <a href="#" class="text-gray-900 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+        <a href="#" class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium">Menu</a>
+        <a href="#" class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium">About Us</a>
+        <a href="#" class="text-gray-700 hover:bg-gray-200 hover:text-gray-900 rounded-md px-3 py-2 text-sm font-medium">Contact</a>
       </div>
     </div>
   </div>
+  <!-- Booking Button -->
+  <div>
+    <a href="#" class="bg-red-800 text-white rounded-md px-4 py-2 text-sm font-medium">Book a Table</a>
+  </div>
 </nav>
+
+
+
+
+
+
+
+
 
 
 </body>
