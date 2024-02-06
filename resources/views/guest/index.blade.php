d3b19382236f21f36a3135e4160e68c5d6a02a7d
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-06 11:59:01 +0100

    Change name picture logo and tab icon.

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="icon" href="{{ asset('images/beets-logo.png') }}">
 </head>
 <body>
 <nav class="bg-red-50 w-full flex justify-between items-center px-20 py-4 ">
   <div class="flex items-center ">
-    <img class="h-8 w-auto pr-6" src="{{ asset('images/beets.png') }}" alt="Beets Restaurant">
+    <img class="h-8 w-auto pr-6" src="{{ asset('images/beets-logo.png') }}" alt="Beets Restaurant">
     <div class="hidden sm:block">
       <div class="flex space-x-4">
         <a href="#" class="text-gray-900 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
@@ -24,14 +25,6 @@
     <a href="#" class="bg-red-800 text-white rounded-md px-4 py-2 text-sm font-medium">Book a Table</a>
   </div>
 </nav>
-
-
-
-
-
-
-
-
 
 
 </body>
