271deb2a8c9e96d55b2a40c12870577aadf2d055
Sieglinde Vanwynsberghe <146928269+Sieglindevwb@users.noreply.github.com>
2024-02-07 16:48:40 +0100

    Trying connect info to database.

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,4 +11,18 @@ class ReservationController extends Controller
     {
         return view('guest.create');
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'], 
+            'email' => ['required', 'email'], 
+            'date' => ['required', 'date', 'after_or_equal:today'],
+            'time' => ['required', 'regex:/^([01]?[0-9]|2[0-3]):[0-5][0-9]$/'],
+            'table_id' => 'required',
+            'notes' => 'max:255',
+        ]);
+
+        return redirect()->route('guest.index')->with('success', 'Booked table');
+    }
 }
