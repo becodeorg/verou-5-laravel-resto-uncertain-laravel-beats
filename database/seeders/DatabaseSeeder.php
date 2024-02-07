463753a70cc090917939aad59333087a659ee278
Eduarda <eduardabpetry@gmail.com>
2024-02-07 16:59:22 +0100

    Create seeder for employees

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Employee::factory()->create([
+        'name' => 'Dwight',
+        'email' => 'schrutefarms@scranton.com',
+        'password' => 'ilovebeets',
+         ]);
     }
 }
