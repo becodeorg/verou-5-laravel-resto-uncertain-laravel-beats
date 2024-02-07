77b134f56b3b22876b5cee217270038582820fc9
Eduarda <eduardabpetry@gmail.com>
2024-02-07 16:56:04 +0100

    Write employee model

---
+++ app/Models/Employee.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Employee extends Authenticatable
+{
+    use HasFactory, HasApiTokens, Notifiable;
+
+  
+    protected $fillable = ['username', 'email'];
+
+    protected $hidden = ['password'];
+
+}
